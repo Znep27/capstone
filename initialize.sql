96b0abe9d2b3716fe74969d08fcc587e6c4e052e
Marc Rozenburgh <znep27@hotmail.com>
2023-10-10 19:37:15 -0500

    update

--- initialize.sql
+++ initialize.sql
@@ -2,6 +2,26 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
-  user_name VARCHAR(50),
+  user_name VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
+);
+
+CREATE TABLE albums (
+  album_id INT NOT NULL,
+  artist_id INT NOT NULL,
+  album_title VARCHAR(MAX) NOT NULL,
+  artist VARCHAR(MAX) NOT NULL,
+  release_year INT NOT NULL,
+  genre VARCHAR(50),
+  cover_art VARCHAR(MAX),
+  PRIMARY KEY (album_id)
+);
+
+CREATE TABLE ratings (
+  user_id INT NOT NULL,
+  album_id INT NOT NULL,
+  rating INT NOT NULL,
+  PRIMARY KEY (rating),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (album_id) REFERENCES albums (album_id)
 );
